3544330583fddfc89a6bad3d602336f12c7417aa
JeetSolanki23 <77983731+JeetSolanki23@users.noreply.github.com>
2024-03-15 04:14:27 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,13 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/3D')
-def home():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return 'About'
+def dashboard():
+	    return render_template('index2.html')
+	
+if __name__ =="__main__":
+    app.run(debug=True, host="0.0.0.0")
