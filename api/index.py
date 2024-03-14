77c673705a3e103d4b6cc4dd7818b67e7695a3ad
JeetSolanki23 <77983731+JeetSolanki23@users.noreply.github.com>
2024-03-15 03:53:48 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
+
+@app.route('/3D')
+def home():
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
